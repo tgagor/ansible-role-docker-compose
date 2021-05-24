91bbb116fea16660cfb08cfa84797c4d64bc85cd
Tomasz Gągor <tgagor@gmail.com>
2021-05-24 13:38:49 +0200

    fix(ansible): resolve conflicts from template

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-Ansible role for XXX
-========================
+Ansible role for docker-compose
+===============================
 
-This role will install XXX on compatible platforms.
+This role will install docker-compose on compatible platforms.
 
-[![Molecule Test](https://github.com/tgagor/ansible-role-XXX/actions/workflows/test-and-release.yml/badge.svg)](https://github.com/tgagor/ansible-role-XXX/actions/workflows/test-and-release.yml)
-[![Ansible Galaxy](https://img.shields.io/badge/ansible--galaxy-tgagor-XXX.svg)](https://galaxy.ansible.com/tgagor/XXX)
+[![Molecule Test](https://github.com/tgagor/ansible-role-docker-compose/actions/workflows/test-and-release.yml/badge.svg)](https://github.com/tgagor/ansible-role-docker-compose/actions/workflows/test-and-release.yml)
+[![Ansible Galaxy](https://img.shields.io/badge/ansible--galaxy-tgagor-docker-compose.svg)](https://galaxy.ansible.com/tgagor/docker-compose)
 
 ## Supported systems
 
@@ -13,25 +13,20 @@ This role will install XXX on compatible platforms.
   * 20.04
   * 20.10
   * 21.04
-* Linux Mint
-  * 19.3
-  * 20
-  * 20.1
 * Debian
   * 9 (stretch)
   * 10 (buster)
   * 11 (bullseye)
 * CentOS
   * 7
-  * 8XXX
+  * 8
 * Fedora
-  * 32
   * 33
   * 34
 
 ## Requirements
 
-As XXX's repo is available via HTTPS, role will install `apt-transport-https` if it's not present in the system.
+If `docker_compose_enable_bash_completion` is set to `true`, role will install `bash-completion` package.
 
 ## Role variables
 
@@ -51,9 +46,9 @@ Just add to you playbook, to install it on `localhost`:
   gather_facts: true
   become: yes
   tasks:
-    - name: Install XXX
+    - name: Install docker_compose
       include_role:
-        name: tgagor.XXX
+        name: tgagor.docker_compose
 ```
 
 ## License
--- molecule/default/tests/test_verify_installation.py
+++ molecule/default/tests/test_verify_installation.py
@@ -1,55 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
+
 import testinfra.utils.ansible_runner
-import pytest
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-# def test_repo_configured(host):
-#     f = None
-#
-#     if host.system_info.distribution in ['debian', 'ubuntu']:
-#         f = host.file('/etc/apt/sources.list.d/docker.list')
-#
-#     if host.system_info.distribution in ['centos', 'fedora', 'redhat']:
-#         f = host.file('/etc/yum.repos.d/docker-ce.repo')
-#
-#     assert f.exists
-#     assert f.contains(f'https://download.docker.com/linux/{host.system_info.distribution}')
-#     assert f.contains(host.system_info.distribution)
+def test_package_installed(host):
+    assert host.file('/usr/local/bin/docker-compose').exists
+    assert host.file('/usr/local/bin/docker-compose').is_symlink
+    assert host.file('/usr/local/bin/docker-compose').mode == 0o777
+    assert host.run('docker-compose --version').succeeded
 
 
-# @pytest.mark.parametrize('pkg', [
-#     'docker-ce',
-#     'docker-ce-cli',
-#     'containerd.io',
-# ])
-# def test_package_installed(host, pkg):
-#     assert host.package(pkg).is_installed
-#     assert host.run('docker --version').succeeded
-
-
-# def test_user_configured(host):
-#     assert host.group('docker').exists
-#     assert 'docker' in host.user('dude').groups
-#
-#     # check if user can really call docker commands
-#     with host.sudo('dude'):
-#         assert host.run('docker info').succeeded
-
-
-# @pytest.mark.parametrize('path', [
-#     '/etc/docker/daemon.json',
-#     '/etc/systemd/system/docker.service.d/hosts.conf',
-# ])
-# def test_config_files_exists(host, path):
-#     assert host.file(path).exists
-
-
-# def test_service_is_running(host):
-#     assert host.service('docker').is_enabled
-#     assert host.service('docker').is_running
-#     assert host.socket("unix:///var/run/docker.sock").is_listening
-#     assert host.socket("tcp://127.0.0.1:2375").is_listening
+def test_bash_completion(host):
+    assert host.package('bash-completion').is_installed
+    assert host.file('/etc/bash_completion.d/docker-compose').exists
